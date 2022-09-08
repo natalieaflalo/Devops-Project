9d2ed01084da10b02f51b71dab1aa6fcc6e7f446
LinoyYarkoni <linoyarkoni20@gmail.com>
2022-09-08 14:27:43 +0300

    jsp files

---
+++ Guru_login.java
@@ -0,0 +1,32 @@
+package demotest;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class guru_login extends HttpServlet {
+
+    public guru_login() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if(username.isEmpty() || password.isEmpty() )
+		{
+			RequestDispatcher req = request.getRequestDispatcher("register_3.jsp");
+			req.include(request, response);
+		}
+		else
+		{
+			RequestDispatcher req = request.getRequestDispatcher("register_4.jsp");
+			req.forward(request, response);
+		}
+	}
+
+}
