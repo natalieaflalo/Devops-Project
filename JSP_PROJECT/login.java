eab3edcc42031547539036c57504d5644de6ea50
LinoyYarkoni <linoyarkoni20@gmail.com>
2022-09-08 21:21:04 +0300

    jsp re-adding
    Co-Authored-By: natalieaflalo <86556267+natalieaflalo@users.noreply.github.com>

---
+++ JSP_PROJECT/login.java
@@ -0,0 +1,32 @@
+package demotest;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class login extends HttpServlet {
+
+    public login() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if(username.isEmpty() || password.isEmpty() )
+		{
+			RequestDispatcher req = request.getRequestDispatcher("registerFirst.jsp");
+			req.include(request, response);
+		}
+		else
+		{
+			RequestDispatcher req = request.getRequestDispatcher("registerSecond.jsp");
+			req.forward(request, response);
+		}
+	}
+
+}
